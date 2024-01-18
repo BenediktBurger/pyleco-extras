1563629273f71773db4528a9b59563ddec333717
Benedikt Burger <67148916+BenediktBurger@users.noreply.github.com>
2024-01-18 14:58:10 +0100

    Rework starter gui.

--- pyleco_extras/gui/starter_gui/data/settings.py
+++ pyleco_extras/gui/starter_gui/data/settings.py
@@ -1,68 +1,15 @@
 """
 Module for the Settings dialog class.
 
-Created on Thu Nov 26 19:02:38 2020 by Benedikt Moneke
+Created on Thu Nov 26 19:02:38 2020 by Benedikt Burger
 """
 
-from PyQt6 import QtCore, QtWidgets, uic
-from PyQt6.QtCore import pyqtSlot
+from qtpy import QtWidgets
+
+from pyleco_extras.gui_utils.base_settings import BaseSettings
 
 
-class Settings(QtWidgets.QDialog):
+class Settings(BaseSettings):
     """Define the settings dialog and its methods."""
-
-    def __init__(self):
-        """Initialize the dialog."""
-        # Use initialization of parent class QDialog.
-        super().__init__()
-
-        # Load the user interface file and show it.
-        uic.loadUi("data/Settings.ui", self)
-        self.show()
-
-        # Configure settings.
-        self.settings = QtCore.QSettings()
-        # Convenience list for widgets with value(), SetValue() methods.
-        self.sets = (
-            # name of widget, key of setting, defaultValue, type of data
-            # (self.widget, 'name', 0, int),
-            (self.sbStarterPort, 'starterPort', 11111, int),
-            # TODO add your widgets.
-        )
-        self.readValues()
-
-        # CONNECT BUTTONS.
-        # Define RestoreDefaults button and connect it.
-        restore = QtWidgets.QDialogButtonBox.StandardButton.RestoreDefaults
-        self.pbRestoreDefaults = self.buttonBox.button(restore)
-        self.pbRestoreDefaults.clicked.connect(self.restoreDefaults)
-        # self.pbSavePath.clicked.connect(self.openFileDialog)
-
-    @pyqtSlot()
-    def readValues(self):
-        """Read the stored values and show them on the user interface."""
-        for widget, name, value, typ in self.sets:
-            widget.setValue(self.settings.value(name, defaultValue=value,
-                                                type=typ))
-        # self.leSavePath.setText(self.settings.value('tasksPath', "tasks", type=str))
-
-    @pyqtSlot()
-    def restoreDefaults(self):
-        """Restore the user interface to default values."""
-        for widget, name, value, typ in self.sets:
-            widget.setValue(value)
-        # self.leSavePath.setText("tasks")
-
-    @pyqtSlot()
-    def accept(self):
-        """Save the values from the user interface in the settings."""
-        # is executed, if pressed on a button with the accept role
-        for widget, name, value, typ in self.sets:
-            self.settings.setValue(name, widget.value())
-        # self.settings.setValue('tasksPath', self.leSavePath.text())
-        super().accept()  # make the normal accept things
-
-    # def openFileDialog(self):
-    #     """Open a file path dialog."""
-    #     path = QtWidgets.QFileDialog.getExistingDirectory(self, "Tasks path")
-    #     self.leSavePath.setText(path)
+    def setup_form(self, layout: QtWidgets.QFormLayout) -> None:
+        pass  # nothing here
